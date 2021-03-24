8ccc92cb01a08b652f663aadb4deca26118291cc
James Holdsworth <james@servitor.newcastle.edu.au>
2021-03-24 15:36:11 +1100

    cleaned up some stuff

--- MSD_switched.py
+++ MSD_switched.py
@@ -1,77 +1,41 @@
 """
-Simulate a first order state space model given by
-x_{t+1} = a*x_t + b*u_t + w_t
-y_t = x_t + e_t
-with q and r the standard deviations of w_t and q_t respectively
-
-Jointly estimate the smoothed state trajectories and parameters theta = {a, b, q, r}
-to give p(x_{1:T}, theta | y_{1:T})
-
-GOAL:
-Use a Monte Carlo style approach to perform MPC where the state constraints are satisfied
-with a given probability.
-
-Current set up: Uses MC to give an expected cost and then satisfies,
-Chance state constraints using a log barrier formulation
-Chance state constraints using a log barrier formulation
-Input constraints using a log barrier formulation
-
-Implementation:
-Uses custom newton method to solve
-Uses JAX to compile and run code on GPU/CPU and provide gradients and hessians
+Simulates a mass-spring-damper system in one dimension (2 state system) with random noise on the state transition. 
+Generates measurements of the system corresponding to position and acceleration of the mass, with random noise on each measurement.
+At time t, the system model changes from system1 --> system 2, in the simplest case this is a change in the parameters.
+Given a window of data where the switch occurs at time t, pystan will sample the states, parameters for both systems, and switching time t jointly.
 """
-
-## TODO list
-# refactor the parameters a little bit to put them into a dictionary, so that the same function can be used for the ssm simulator and the jax mpc simulator
-
 
 # general imports
 from numpy.core.numeric import zeros_like
 import pystan
 import numpy as np
 import matplotlib.pyplot as plt
-from helpers import plot_trace, col_vec, row_vec
+from helpers import plot_trace
 from pathlib import Path
 import pickle
 
-# jax related imports
-import jax.numpy as jnp
-from jax import grad, jit, device_put, jacfwd, jacrev
-from jax.ops import index, index_add, index_update
-from jax.config import config
-
-# optimisation module imports (needs to be done before the jax confix update)
-# from optimisation import log_barrier_cost, solve_chance_logbarrier
-
-config.update("jax_enable_x64", True)           # run jax in 64 bit mode for accuracy
-
-
+plot_bool = False
 #----------------- Parameters ---------------------------------------------------#
 
-Ns = 200             # number of samples we will use for MC MPC
-
-# simulation parameters
 T = 100             # number of time steps to simulate and record measurements for
-z1_0 = 3.0            # initial x
-z2_0 = 0.0
-r1_true = 0.1        # measurement noise standard deviation
+
+# true (simulation) parameters
+z1_0 = 3.0  # initial position
+z2_0 = 0.0  # initial velocity
+r1_true = 0.1 # measurement noise standard deviation
 r2_true = 0.01
-q1_true = 0.05       # process noise standard deviation
-q2_true = 0.005      # process noise standard deviation
+q1_true = 0.05 # process noise standard deviation
+q2_true = 0.005 
 m_true = 2
 b_true = 0.7
 k_true = 0.25
 
-Nx = 2;
-Ny = 2;
-Nu = 1;
+Nx = 2; # number of states
+Ny = 2; # number of measurements
+Nu = 1; # number of inputs
 
 #----------------- Simulate the system-------------------------------------------#
 
-# def ssm1(x1, x2, u, T, a11=0.0, a12=1.0, b1=0.0):
-#     return (a11*x1 + a12*x2 + b1*u)*T
-# def ssm2(x1, x2, u, T, a21=0.5, a22=-0.5, b2=0.3):
-#     return a21*x1 + a22*x2 + b2*u
 def ssm_euler(x,u,A,B,T):
     return (np.matmul(A,x) + np.matmul(B,u)) * T;
 
@@ -87,59 +51,60 @@ A1[1,0] = -k_true/m_true
 A1[1,1] = -b_true/m_true
 B1[1,0] = 1/m_true;
 
-# mass doubled!
+# mass doubled for system 2
 A2[0,1] = 1.0;
 A2[1,0] = -k_true/m_true/2
 A2[1,1] = -b_true/m_true/2
 B2[1,0] = 1/m_true/2;
 
+z_sim = np.zeros((Nx,T+1), dtype=float) # state history allocation
 
-
-z_sim = np.zeros((Nx,T+1), dtype=float) # state history
-
-# initial state
+# load initial state
 z_sim[0,0] = z1_0 
 z_sim[1,0] = z2_0 
 
-# noise predrawn and independant
+# noise is predrawn and independant
 w_sim = np.zeros((Nx,T),dtype=float)
 w_sim[0,:] = np.random.normal(0.0, q1_true, T)
 w_sim[1,:] = np.random.normal(0.0, q2_true, T)
 
-# create some inputs that are random but held for 10 time steps
+# create some inputs that are random
 u = np.random.uniform(-0.5,0.5, T*Nu)
 u = np.reshape(u, (Nu,T))
 
-# spicy
-t_fail = 100 # no failure at 220
+# time of switch
+t_switch = 33
 for k in range(T):
     # x1[k+1] = ssm1(x1[k],x2[k],u[k]) + w1[k]
     # x2[k+1] = ssm2(x1[k],x2[k],u[k]) + w2[k]
-    if k<t_fail:
+    if k<t_switch:
         z_sim[:,k+1] = z_sim[:,k] + ssm_euler(z_sim[:,k],u[:,k],A1,B1,1.0) + w_sim[:,k]
     else:
         z_sim[:,k+1] = z_sim[:,k] + ssm_euler(z_sim[:,k],u[:,k],A2,B2,1.0) + w_sim[:,k]
 
-# simulate measurements
+# draw measurement noise
 v = np.zeros((Ny,T), dtype=float)
 v[0,:] = np.random.normal(0.0, r1_true, T)
 v[1,:] = np.random.normal(0.0, r2_true, T)
+
+# simulated measurements 
 y = np.zeros((Ny,T), dtype=float)
 y[0,:] = z_sim[0,:-1]
-y[1,:t_fail] = (-k_true*z_sim[0,:t_fail] -b_true*z_sim[1,:t_fail] + u[0,:t_fail])/m_true
-y[1,t_fail:] = (-k_true*z_sim[0,t_fail:-1] -b_true*z_sim[1,t_fail:-1] + u[0,t_fail:])/(m_true*2)
-y = y + v; # add noise
+y[1,:t_switch] = (-k_true*z_sim[0,:t_switch] -b_true*z_sim[1,:t_switch] + u[0,:t_switch])/m_true
+y[1,t_switch:] = (-k_true*z_sim[0,t_switch:-1] -b_true*z_sim[1,t_switch:-1] + u[0,t_switch:])/(m_true*2)
+y = y + v; # add noise to measurements
 
-plt.subplot(2,1,1)
-plt.plot(u[0,:])
-plt.plot(y[1,:],linestyle='None',color='r',marker='*')
-plt.title('Simulated inputs and measurement used for inference')
-plt.subplot(2, 1, 2)
-plt.plot(z_sim[0,:])
-plt.plot(y[0,:],linestyle='None',color='r',marker='*')
-plt.title('Simulated state 1 and measurements used for inferences')
-plt.tight_layout()
-plt.show()
+if plot_bool:
+    plt.subplot(2,1,1)
+    plt.plot(u[0,:])
+    plt.plot(y[1,:],linestyle='None',color='r',marker='*')
+    plt.title('Simulated inputs and measurement used for inference')
+    plt.subplot(2, 1, 2)
+    plt.plot(z_sim[0,:])
+    plt.plot(y[0,:],linestyle='None',color='r',marker='*')
+    plt.title('Simulated state 1 and measurements used for inferences')
+    plt.tight_layout()
+    plt.show()
 
 #----------- USE HMC TO PERFORM INFERENCE ---------------------------#
 # avoid recompiling
